eb9923eae03cad066ac31e61d9744a6025f6fcaa
AlexitoReyes54 <alexandereliasr9@gmail.com>
2026-02-09 17:02:41 -0400

    feat: first steps to validate yaml bussines logic file

--- src/BussinesLogicParser/BussinesLogicParser.ts
+++ src/BussinesLogicParser/BussinesLogicParser.ts
@@ -1,1 +1,24 @@
-console.log("yaml file parser");
+import { YAML } from "bun";
+import init from "./init.yml"
+
+class BussinesLogicParser {
+
+	loadYAML() {
+		let init_ = YAML.parse(init)
+		//
+		let s = JSON.stringify(init)
+		console.dir(init, {
+			depth: null,
+			colors: true
+		})
+
+	}
+}
+
+// TODO just use zod package for the validation is going to be faster and maybe esier to maintain sence its lesss code 
+// that i hace to work on
+//
+let parser = new BussinesLogicParser();
+parser.loadYAML()
+
+export default BussinesLogicParser;
